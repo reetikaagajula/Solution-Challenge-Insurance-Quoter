2f5b290522dd288ce2f618faf8a976d90e435991
ReetikaaGajula2002 <rgajula@umd.edu>
2025-04-03 19:31:31 -0400

    Initial commit with Docker setup and frontend/backend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Life Insurance Voice Assistant</h1>
+      <p>This is your React frontend running inside Docker!</p>
+    </div>
+  );
+}
+
+export default App;
